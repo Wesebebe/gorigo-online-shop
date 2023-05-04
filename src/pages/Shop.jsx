3f46cbe1c263f9ac9d66c57d7bb4cce2a1916528
gorety <wagutugorety@gmail.com>
2023-05-04 11:06:21 +0300

    changes in shop.css

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Product from '../components/Product';
+import './Shop.css'
 
 function Shop() {
+
+    const [items, setItems] =useState([])
+
+    useEffect(()=>{
+        fetch ('http://localhost:3001/items')
+        .then((resp) => resp.json())
+        .then((items) => setItems(items))
+
+    }, []);
+
+    const List =items.map((item) => {
+        return(
+            <Product key={item.id} name={item.name} price={item.price} src={item.src}/>
+        )
+    })
     return (
-        <div>Shop</div>
+        <div className="products">
+            {List}
+            </div>
     )
 }
 
