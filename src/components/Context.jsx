726e751f6109d02d20939c2b084b573222f45165
gorety <wagutugorety@gmail.com>
2023-05-04 13:27:43 +0300

    Changes in context.jsx

--- src/components/Context.jsx
+++ src/components/Context.jsx
@@ -10,12 +10,23 @@ const getDefaultCart = () => {
     return Cart;
 }
 
-function ShopContextProvidr(props) {
+function ShopContextProvider(props) {
 
     const [cartItems,setCartItems] = useState(getDefaultCart);
+    const addToCart = (itemId) => {
+        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+
+        const removeFromCart = (itemId) => {
+            setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
+    };
+
+    const contexValue = {cartItems, addToCart, removeFromCart};
   return (
-    <ShopContext.Provider> {props.children}</ShopContext.Provider>
+    <ShopContext.Provider value={contexValue} > 
+        {props.children}
+    </ShopContext.Provider>
   )
+}
 }
 
 export default Context;
